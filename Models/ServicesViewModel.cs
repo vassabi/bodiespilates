ca341ad317b4fc1cf2ec59b7381c0330cee6e723
karen_aghajanyan <karen.aghajanyan@gmail.com>
2022-03-20 17:01:26 +0400

    Missing models and views

---
+++ Models/ServicesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ServicesViewModel
+    {
+        public SubscribeFormViewModel SubscribeModel { get; set; }
+        public FAQViewModel FaqModel { get; set; }
+    }
+}
---
+++ Models/SubscribeFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class SubscribeFormViewModel
+    {
+        public string SelectedPlan { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool AgreeTerms { get; set; }
+    }
+}
